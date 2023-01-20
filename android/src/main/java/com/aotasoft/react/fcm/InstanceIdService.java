93febedef8aba6a3bb10a45dc0ed3fe5bd83d058
Veekshith Kajeepeta <veekshith.k@gmail.com>
2023-01-20 16:08:12 -0600

    Add FCM base files and messaging services

---
+++ android/src/main/java/com/aotasoft/react/fcm/InstanceIdService.java
@@ -0,0 +1,35 @@
+package com.aotasoft.react.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "InstanceIdService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // Broadcast refreshed token
+
+        Intent i = new Intent("com.aotasoft.react.fcm.FCMRefreshToken");
+        Bundle bundle = new Bundle();
+        bundle.putString("token", refreshedToken);
+        i.putExtras(bundle);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(i);
+    }
+}
